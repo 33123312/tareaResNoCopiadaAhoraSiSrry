0caefacff68d791bdda8a20a95a6b3205ca37103
33123312 <65249051+33123312@users.noreply.github.com>
2022-06-06 20:35:34 -0600

    segunda iplementacion no copiada :´v

--- rest/rest/OrderPage.xaml.cs
+++ rest/rest/OrderPage.xaml.cs
@@ -16,5 +16,22 @@ namespace rest
         {
             InitializeComponent();
         }
+
+        private void CarouselPositionChanged(object sender, PositionChangedEventArgs e)
+        {
+            var carousel = sender as CarouselView;
+            var views = carousel.VisibleViews;
+
+            if (views.Count > 0)
+            {
+                foreach (var view in views)
+                {
+                    var img = view.FindByName<Image>("MenuImg");
+                    ViewExtensions.CancelAnimations(img);
+
+                    Task.Run(async () => await img.RelRotateTo(360, 5000, Easing.BounceOut));
+                }
+            }
+        }
     }
 }
